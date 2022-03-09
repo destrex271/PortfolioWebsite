37e06bfaaaa8280e73a521c85e770356f33f567d
destrex271 <destrex271@gmail.com>
2022-03-09 14:58:37 +0530

    Terminal header

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Landing(){
+    return(
+        <React.Fragment>
+            <div className="container-fluid mt-2 landing-bg">
+                <div className="display-2 head-font">Hello</div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Landing;
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+function Navbar(){
+    return(
+        <React.Fragment>
+            <nav className="container-fluid d-flex justify-content-around navbox sticky-top">
+                <ul class="nav container-fluid d-flex justify-content-around">
+                    <li class="nav-item hover-underline-animation">
+                        <a class="nav-link" aria-current="page" href="#">About Me</a>
+                    </li>
+                    <li class="nav-item hover-underline-animation">
+                        <a class="nav-link" href="#">Projects</a>
+                    </li>
+                    <li class="nav-item hover-underline-animation">
+                        <a class="nav-link" href="#">Research Papers</a>
+                    </li>
+                    <li class="nav-item hover-underline-animation">
+                        <a class="nav-link" href="#">Contacts</a>
+                    </li>
+                </ul>
+            </nav>
+        </React.Fragment>
+    )
+}
+
+export default Navbar;
