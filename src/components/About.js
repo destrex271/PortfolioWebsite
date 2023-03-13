19de15f4e27c22af8a5334e89f59f9bd2351a431
Akshat Jaimini <destrex271@gmail.com>
2023-03-13 16:23:42 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,11 @@ function About(){
                         <img src={Dev} width="50%" className="ts"/>
                     </div>
                     <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 content-font-about mt-5">
-                    I am <span style={{color:"#ffffff"}} className="glow">Akshat Jaimini</span>, a freshman at Thapar Institute Of Engineering And Technology, Patiala.<br/>I am an aspiring <span style={{color:"#ffffff"}} className="glow">Software Developer</span> and An <span style={{color:"#ffffff"}} className="glow">Open Source Enthusiast</span>.Improving human interactions with computers and all electronic devices excites me the most.<br/>Currently I am intrested in learning more about <span style={{color:"#ffffff"}} className="glow">Game Developement</span> and the 3-D industry in general alongwith some work in mobile and web developement.<br/><br/>I am working with <span style={{color:"#ffffff"}} className="glow">React.js</span> for frontend developement, <span style={{color:"#ffffff"}} className="glow">Django</span> for Backend Web Developement, <span style={{color:"#ffffff"}} className="glow">React Native</span> for mobile app developement.<br/>I am also learning game developement in <span style={{color:"#ffffff"}} className="glow">Unity 3D</span>.
+                    I am <span style={{color:"#ffffff"}} className="glow">Akshat Jaimini</span>, a student at Thapar Institute Of Engineering And Technology, Patiala.<br/>
+                        I am an aspiring <span style={{color:"#ffffff"}} className="glow">Software Engineer</span> and An <span style={{color:"#ffffff"}} className="glow">Open Source Enthusiast</span>.
+                        Improving human interactions with computers and all electronic devices excites me the most.<br/>
+                            Currently I am intrested in learning more about <span style={{color:"#ffffff"}} className="glow">Systems Programming</span> and 
+<span style={{color:"#ffffff"}} className="glow">Computer Graphics</span>.<br/><br/>
                     </div>
                 </div>
             {/* </div> */}
